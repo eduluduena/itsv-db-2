b06d4885373804e3c1186b8a424621c6ee675fb2
eduluduena <31923566+eduluduena@users.noreply.github.com>
2020-04-21 21:34:28 -0300

    Update class_06.sql

--- exercises/class_06.sql
+++ exercises/class_06.sql
@@ -1,32 +1,32 @@
-#Ej N�1
+#Ej N°1
 SELECT * FROM actor a WHERE EXISTS (SELECT * FROM actor aa WHERE a.last_name = aa.last_name) ORDER  BY a.last_name
 
-#Ej N�2
+#Ej N°2
 #No Me Salio
 
 #Ej N°3
 SELECT * FROM customer c WHERE (SELECT COUNT(*) FROM rental rr WHERE rr.customer_id = c.customer_id) = 1;
 
-#Ej N�4
+#Ej N°4
 SELECT * FROM customer c WHERE (SELECT COUNT(*) FROM rental rr, customer cc WHERE rr.customer_id = cc.customer_id) > 1
 
-#Ej N�5
+#Ej N°5
 SELECT * FROM actor a, film_actor fa , film f WHERE a.actor_id=fa.actor_id
 	AND f.film_id = fa.film_id AND (f.title = "BETRAYED REAR" OR f.title = "CATCH AMISTAD");
 	
-#Ej N�6
+#Ej N°6
 SELECT * FROM actor a, film_actor fa , film f WHERE a.actor_id=fa.actor_id
 	AND f.film_id=fa.film_id AND f.title="BETRAYED REAR" AND a.actor_id NOT IN (
 		SELECT aa.actor_id from actor aa, film_actor faa, film ff WHERE aa.actor_id = faa.actor_id 
 			AND ff.film_id=faa.film_id AND ff.title="CATCH AMISTAD");
 
-#Ej N�7
+#Ej N°7
 SELECT * FROM actor a, film_actor fa , film f WHERE a.actor_id=fa.actor_id
 	AND f.film_id=fa.film_id AND f.title="BETRAYED REAR" AND a.actor_id IN (
 		SELECT aa.actor_id from actor aa, film_actor faa, film ff WHERE aa.actor_id = faa.actor_id 
 			AND ff.film_id=faa.film_id AND ff.title="CATCH AMISTAD");
 
 
-#Ej N�8
+#Ej N°8
 SELECT * FROM actor a, film_actor fa , film f WHERE a.actor_id=fa.actor_id
 	AND f.film_id = fa.film_id AND (f.title != "BETRAYED REAR" OR f.title != "CATCH AMISTAD");
